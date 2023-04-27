f28d140df60c678f40881fa681fd7651b09b0a9a
mahaleashish <ashishmahale555@gmail.com>
2023-04-28 04:23:38 +0530

    Name updater assignmment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Counter from './component/Counter';
+import NameUpdater from './component/NameUpdater';
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <div className="App">
-            <Counter />
+            <NameUpdater />
         </div>
     );
 }
