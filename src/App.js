66e6b4eb94b35466dc75fb4c95809f9179ae2bb7
mahaleashish <ashishmahale555@gmail.com>
2023-05-03 01:35:30 +0530

    This is assignment on fetching API for user cards

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from 'react';
-//useState to store current dog img url
-//useEffect to fetch a new dog img on the initial page load
+import React, { useState } from 'react';
+import './App.css';
 
-const App = () => {
-    const [DogImage, setDogImage] = useState('');
+function App() {
+    const [users, setUsers] = useState([]);
 
-//func fetchDogImage uses fetch to make a req. to dog API
-
-    const fetchDogImage = async () => {
-        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const getUsers = async () => {
+        const response = await fetch('https://reqres.in/api/users/');
         const data = await response.json();
-        setDogImage(data.message);
+        setUsers(data.data);
     };
 
-    useEffect(() => {
-        fetchDogImage();
-    }, []);
-
-
-//useEffect with an empty dependency arr[] to run only on the initial page load.
-
     return (
-        <div>
-            <img src={DogImage} alt="dog" />
+        <div className="App">
+            <button onClick={getUsers}>Get Users</button>
+            <div className="user-cards">
+                {users.map((user) => (
+                    <div className="user-card" key={user.id}>
+                        <img src={user.avatar} alt="user-avatar" />
+                        <h2>{`${user.first_name} ${user.last_name}`}</h2>
+                        <p>{user.email}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
-};
+}
 
 export default App;
