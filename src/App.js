8a5100a404c2ef90fa59fb3a5e56ea6a891f52e1
mahaleashish <ashishmahale555@gmail.com>
2023-05-03 00:10:35 +0530

    This is fetch async await assignment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import './App.css';
-import Useref from './Useref';
+import React, { useState, useEffect } from 'react';
+//useState to store current dog img url
+//useEffect to fetch a new dog img on the initial page load
 
-function App() {
+const App = () => {
+    const [DogImage, setDogImage] = useState('');
 
+//func fetchDogImage uses fetch to make a req. to dog API
+
+    const fetchDogImage = async () => {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        setDogImage(data.message);
+    };
+
+    useEffect(() => {
+        fetchDogImage();
+    }, []);
+
+
+//useEffect with an empty dependency arr[] to run only on the initial page load.
 
     return (
-        <div className="App">
-            <Useref />
+        <div>
+            <img src={DogImage} alt="dog" />
         </div>
     );
-}
+};
 
 export default App;
