8e91cd044719b53edab918ac7b93bcc883215e15
mahaleashish <ashishmahale555@gmail.com>
2023-05-12 03:35:41 +0530

    Tic tac toe game code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import ScrollBar from './atom/molecules/scrollbar/Scrollbar';
+import Tic from './atom/molecules/tictactoe/Tic';
 
 function App() {
   return (
           <>
-      <ScrollBar />
+      <Tic/>
           </>   
   );
 }
